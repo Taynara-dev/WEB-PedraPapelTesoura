9568071c824c577248a5055707d712b70cc4015c
Augusto Manzoni Mariani <augusto.m26@aluno.ifsc.edu.br>
2021-11-29 11:33:49 -0300

    jogada do computador correção + mudança da exibição das opções e placar

--- script.js
+++ script.js
@@ -3,82 +3,86 @@ let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
+const options_p = document.querySelector(".options > p");
 const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById ("r");
 const paper_div = document.getElementById ("p");
 const scissors_div = document.getElementById ("s");
 
-function getComputerChoice(){
+function getComputerChoice() {
     const choices = ['r', 'p', 's'];
-    const randomNumber = Math.floor(Math.random() * 3 );
+    const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 
 function convertToWorld(letter){
-    if (letter === "r") return "Pedra";
-    if (letter === "p") return "Papel";
-    return "Tesoura";
+    if (letter === "r") return "Pedra".fontsize(6).fontcolor('lightgray');
+    if (letter === "p") return "Papel".fontsize(6).fontcolor('lightgray');
+    return "Tesoura".fontsize(6).fontcolor('lightgray');
 }
 
-function win(userChoice, computerChoice){
-    const smallUserWord = "user".fontsize(3).sub();
-    const smallCompWord = "comp".fontsize(3).sub();
+function ganhar(userChoice, computerChoice){
+    const smallUserWord = "Usuario: ";
+    const smallCompWord = "Computador: ";
     const userChoice_div = document.getElementById(userChoice);
     userScore++;
     userScore_span.innerHTML = userScore;
-    computerScore_span = computerScore;
-    result_p.innerHTML = `${convertToWorld(userChoice)}${smallUserWord} beats ${convertToWorld(computerChoice)}${smallCompWord} . Você ganhou! `;
-    userChoice_div.classList.add('.green-glow');
+    computerScore_span.innerHTML = computerScore;
+    options_p.innerHTML = `${smallUserWord}${convertToWorld(userChoice)} ${" ganha de ".fontcolor('gray').italics()} ${smallCompWord}${convertToWorld(computerChoice)} `.fontsize(6);
+    result_p.innerHTML = "Você Venceu! ".fontcolor('greenyellow');
+    userChoice_div.classList.add('green-glow');
     setTimeout(() => userChoice_div.classList.remove('green-glow'), 300);
 }
 
 
-function lose(userChoice, computerChoice){
+function perder(userChoice, computerChoice){
     computerScore++;
     userScore_span.innerHTML = userScore;
-    computerScore_span = computerScore;
-    const smallUserWord = "user".fontsize(3).sub();
-    const smallCompWord = "comp".fontsize(3).sub();
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "Usuario: ";
+    const smallCompWord = "Computador: ";
     const userChoice_div = document.getElementById(userChoice);
-    result_p.innerHTML = `${convertToWorld(userChoice)}${smallUserWord} loses to ${convertToWorld(computerChoice)}${smallCompWord} . Você perdeu... `;
-    userChoice_div.classList.add('.red-glow');
+    options_p.innerHTML = `${smallUserWord}${convertToWorld(userChoice)} ${" perde de ".fontcolor('gray').italics()} ${smallCompWord}${convertToWorld(computerChoice)} `.fontsize(6);
+    result_p.innerHTML = "Você perdeu!".fontcolor('tomato');
+    userChoice_div.classList.add('red-glow');
     setTimeout(() => userChoice_div.classList.remove('red-glow'), 300);
 }
 
-function draw(userChoice, computerChoice){
-    const smallUserWord = "user".fontsize(3).sub();
-    const smallCompWord = "comp".fontsize(3).sub();
+function empatar(userChoice, computerChoice){
+    const smallUserWord = "Usuario: ";
+    const smallCompWord = "Computador: ";
     const userChoice_div = document.getElementById(userChoice);
-    result_p.innerHTML = `${convertToWorld(userChoice)}${smallUserWord} igual a ${convertToWorld(computerChoice)}${smallCompWord} . Empate! `;  
-    userChoice_div.classList.add('.gray-glow');
+    options_p.innerHTML = `${smallUserWord}${convertToWorld(userChoice)} ${" igual a ".fontcolor('gray').italics()} ${smallCompWord}${convertToWorld(computerChoice)} `.fontsize(6);  
+    result_p.innerHTML = "Empate! ".fontcolor('cornflowerblue');
+    userChoice_div.classList.add('gray-glow');
     setTimeout(() => userChoice_div.classList.remove('gray-glow'), 300);
 }
 
-function game(userChoice){
+function jogo(userChoice){
     const computerChoice = getComputerChoice();
-    switch (userChoice + computerChoice){
+    switch (userChoice + computerChoice) {
         case "rp":
         case "pr":
         case "sp":
-            win(userChoice, computerChoice);
+            ganhar(userChoice, computerChoice);
             break;
         case "rp":
         case "ps":
         case "sr":
-            lose(userChoice, computerChoice);
+            perder(userChoice, computerChoice);
             break;
         case "rr":
         case "pp":
         case "ss":
-            draw(userChoice, computerChoice);
+            empatar(userChoice, computerChoice);
             break;
     }
 }
 
 function main(){
-rock_div.addEventListener('click', () =>   game("r"));
-paper_div.addEventListener('click', () =>    game("p"));
-scissors_div.addEventListener('click', ()  =>  game("s"));
+rock_div.addEventListener('click', () =>   jogo("r"));
+paper_div.addEventListener('click', () =>    jogo("p"));
+scissors_div.addEventListener('click', ()  =>  jogo("s"));
 }
 
 main();
